4abd78db4d5b472fdcdcf24e159ffea06bf69e5a
Pratiksha <pratzzz2432@gmail.com>
2025-03-02 11:51:25 +0530

    refugee camp  update

--- lib/app/modules/refugee_camp/views/refugee_camp_page.dart
+++ lib/app/modules/refugee_camp/views/refugee_camp_page.dart
@@ -1,15 +1,16 @@
+
 import 'package:flutter/material.dart';
+import 'package:d_m/app/common/widgets/common_scaffold.dart';
 
 class RefugeeCampPage extends StatelessWidget {
   const RefugeeCampPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Refugee Camp')),
-      body: const Center(
-        child: Text('Information about Refugee Camp locations.'),
-      ),
+    return CommonScaffold(
+      title: 'Refugee Camp',
+      currentIndex: 1,
+      body: Center(child: Text('Refugee Camp Information')),
     );
   }
 }
