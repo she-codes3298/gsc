2b2241ca0a0ca28b8a9f81b9d40572f4d1c9e69f
pratzzz2432 <142898815+pratzzz2432@users.noreply.github.com>
2025-06-08 10:24:04 +0530

    UI-UX

--- lib/app/central/modules/ai_chatbot.dart
+++ lib/app/central/modules/ai_chatbot.dart
@@ -3,7 +3,6 @@ import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-import 'dart:typed_data';
 import 'package:gsc/app/central/common/translatable_text.dart';
 
 class AIChatbotScreen extends StatefulWidget {
@@ -12,6 +11,11 @@ class AIChatbotScreen extends StatefulWidget {
   @override
   _AIChatbotScreenState createState() => _AIChatbotScreenState();
 }
+
+const Color darkBackground = Color(0xFF121212);
+const Color darkChatBubble = Color(0xFF424242);
+const Color darkInputBackground = Color(0xFF1E1E1E);
+const Color darkPromptButton = Color(0xFF4A4A4A);
 
 class _AIChatbotScreenState extends State<AIChatbotScreen> {
   final ChatUser _user = ChatUser(id: '1', firstName: 'User');
@@ -145,6 +149,7 @@ class _AIChatbotScreenState extends State<AIChatbotScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: darkBackground,
       appBar: AppBar(
         title: const Text('E-Sahyog', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.black,
@@ -153,7 +158,6 @@ class _AIChatbotScreenState extends State<AIChatbotScreen> {
           onPressed: () => Navigator.pop(context),
         ),
       ),
-      backgroundColor: Colors.grey[900],
       body: Stack(
         children: [
           DashChat(
@@ -161,15 +165,18 @@ class _AIChatbotScreenState extends State<AIChatbotScreen> {
             messages: _messages,
             typingUsers: _isTyping ? [_bot] : [],
             messageOptions: MessageOptions(
-              containerColor: const Color.fromARGB(255, 66, 66, 66),
+              containerColor: darkChatBubble,
               textColor: Colors.white,
-              currentUserContainerColor: Colors.blue,
+              currentUserContainerColor: Colors.blue[800]!,
+              currentUserTextColor: Colors.white,
             ),
             inputOptions: InputOptions(
               inputTextStyle: const TextStyle(color: Colors.white),
               inputDecoration: InputDecoration(
-                fillColor: Colors.black,
+                fillColor: darkInputBackground,
                 filled: true,
+                hintText: 'Type your message...',
+                hintStyle: const TextStyle(color: Colors.grey),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(20),
                   borderSide: const BorderSide(color: Colors.grey, width: 2),
@@ -182,8 +189,6 @@ class _AIChatbotScreenState extends State<AIChatbotScreen> {
                   borderRadius: BorderRadius.circular(20),
                   borderSide: const BorderSide(color: Colors.white, width: 2),
                 ),
-                hintText: 'Type your message...',
-                hintStyle: const TextStyle(color: Colors.grey),
               ),
               trailing: [
                 IconButton(
@@ -203,9 +208,9 @@ class _AIChatbotScreenState extends State<AIChatbotScreen> {
               child: Container(
                 height: 100,
                 decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.8),
+                  color: Colors.black.withOpacity(0.85),
                   borderRadius: const BorderRadius.vertical(
-                    top: Radius.circular(10),
+                    top: Radius.circular(16),
                   ),
                 ),
                 child: ListView.builder(
@@ -213,17 +218,16 @@ class _AIChatbotScreenState extends State<AIChatbotScreen> {
                   itemCount: _customPrompts.length,
                   itemBuilder: (context, index) {
                     return Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 6,
+                        vertical: 12,
+                      ),
                       child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey[700],
+                          backgroundColor: darkPromptButton,
                           foregroundColor: Colors.white,
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(20),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 16,
-                            vertical: 8,
                           ),
                         ),
                         onPressed: () {
@@ -237,7 +241,7 @@ class _AIChatbotScreenState extends State<AIChatbotScreen> {
                         },
                         child: Text(
                           _customPrompts[index],
-                          style: const TextStyle(fontSize: 14),
+                          style: const TextStyle(fontSize: 13),
                         ),
                       ),
                     );
